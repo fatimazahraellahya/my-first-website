b2f6e20c851a4e055b59dc86365e3317167bcea5
fatimazahraellahya <87916645+fatimazahraellahya@users.noreply.github.com>
2021-07-24 23:37:05 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# my-first-website
-Created with CodeSandbox
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hi I'm Fatima Zahra, I just started my coding journey by learning
+          Next.Js and this is my first website that I built using{" "}
+          <a href="https://nextjs.org/learn">Next Js tutorial</a>
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
